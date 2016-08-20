8e24f501cf812b6954504ca92afa63408bd2b05a
pritam shrestha <safal_pritam@hotmail.com>
2016-08-20 10:40:04 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+ //this is for the javascript
+ $(document).ready(function(){
+ 	$('#btn').click(function(){
+ 		$('#colid').toggle()
+ 	})
+ 	$('#btn1').click(function(){
+ 		$('#colid1').toggle()
+ 	})
+ 	$('#btn2').click(function(){
+ 		$('#colid2').toggle()
+ 	})
+ 	$('#btn3').click(function(){
+ 		$('#colid3').toggle()
+ 	})
+
+ }) 
+  
